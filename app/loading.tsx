1641af11ddefe5aa211d4dacc296b2b2de11ed92
David FÁBRY <david.fabry@tipsport.cz>
2023-10-13 13:40:10 +0200

    error handling

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Loding = () => {
+const Loading = () => {
   return <span className="loading loading-spinner loading-md" />;
 };
 
-export default Loding;
+export default Loading;
