4c41cabc1b0fcc0b759f451fa395b91bd7a1dae2
David FÁBRY <david.fabry@tipsport.cz>
2023-10-14 11:25:57 +0200

    odhlaseni

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <ProductCard />
     </>
   );
