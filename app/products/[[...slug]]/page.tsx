31fb9e7aee2ab5e26260e3258f2954449fd9e429
David FÁBRY <david.fabry@tipsport.cz>
2023-10-14 14:26:14 +0200

    lazy loading

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import _ from "lodash";
 
 interface Props {
   params: { slug: string[] };
@@ -12,6 +14,16 @@ const ProductPage = ({
   return (
     <div>
       ProductPage {slug} {sortOrder}
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "c" }, { name: "a" }, { name: "b" }];
+          const sortedArray = _.orderBy(users, ["name"]);
+          console.log(sortedArray);
+        }}
+      >
+        Show
+      </button>
     </div>
   );
 };
