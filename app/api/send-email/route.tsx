0e68833a45f53eb5fa1e6f5feb0d6fbda41cbecd
David FÁBRY <david.fabry@tipsport.cz>
2023-10-15 08:16:26 +0200

    remove code for sending emails

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -2,15 +2,15 @@ import WelcomeTemplate from "@/emails/WelcomeTemplate";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  await resend.emails.send({
-    from: "...",
-    to: "fabry.d@seznam.cz",
-    subject: "Subjekt",
-    react: <WelcomeTemplate name="David" />,
-  });
+  // await resend.emails.send({
+  //   from: "...",
+  //   to: "fabry.d@seznam.cz",
+  //   subject: "Subjekt",
+  //   react: <WelcomeTemplate name="David" />,
+  // });
 
   return NextResponse.json({});
 }
